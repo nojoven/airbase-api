c6f0ffb32a5b0d257178d335a43ae5fea7f9a859
aymerickvalette <valaymerick@gmail.com>
2018-03-29 00:10:41 +0200

    Start the server after connecting the database

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
 const routes = require('./app/routes');
 
-app.use('/', routes)
+app.use('/', routes);
 
-app.listen(process.env.PORT);
+/**
+ * Init the database connection and start the server
+ */
+MongoClient.connect("mongodb://heroku_8618v047:s1s95q848cfnk2gi07ksuri2ov@ds119018.mlab.com:19018/heroku_8618v047", (err, db) => {
+    if (err) throw err;
+
+    app.locals.db = db;
+    app.listen(3000);
+});
