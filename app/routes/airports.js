933ed3ac9e01260380f2eae8226348a32074a78a
aymerickvalette <valaymerick@gmail.com>
2018-03-28 13:46:24 +0200

    Formatting changes

--- app/routes/airports.js
+++ app/routes/airports.js
@@ -1,11 +1,11 @@
 const mongoclient = require('mongodb').MongoClient;
 
 function getAllAirports(req, res) {
-  mongoclient.connect('mongodb://localhost:27017/airbase', function(err, client) {
+  mongoclient.connect('mongodb://localhost:27017/airbase', function (err, client) {
     if (err) {
       throw err;
     }
-    client.db('airbase').collection('airports').find().toArray(function(err, result) {
+    client.db('airbase').collection('airports').find().toArray(function (err, result) {
       if (err) {
         throw err;
       }
