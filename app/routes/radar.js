f73b4e73465304120426d5b70e52ac8ac7aee9dc
aymerickvalette <valaymerick@gmail.com>
2018-03-28 13:46:47 +0200

    Add radar algorithm

---
+++ app/routes/radar.js
@@ -0,0 +1,34 @@
+const mongoclient = require('mongodb').MongoClient;
+
+function getAirportsRadar(req, res) {
+    let coords = req.query.coords.split(",");
+
+    let latRange = [parseFloat(coords[0]) - 0.1, parseFloat(coords[0]) + 0.1];
+    let lonRange = [parseFloat(coords[1]) - 0.1, parseFloat(coords[1]) + 0.1];
+
+
+    mongoclient.connect('mongodb://localhost:27017/airbase', function (err, client) {
+        if (err) {
+            throw err;
+        }
+        client.db('airbase').collection('airports').find({
+            $and: [{
+                lat: {
+                    $gt: latRange[0],
+                    $lt: latRange[1]
+                }, 
+                lon: {
+                    $gt: lonRange[0],
+                    $lt: lonRange[1]
+                }, 
+            }]
+        }).toArray(function (err, result) {
+            if (err) {
+                throw err;
+            }
+            res.json(result);
+        });
+    });
+}
+
+module.exports = getAirportsRadar;
