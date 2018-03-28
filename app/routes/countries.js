02c43e491fbeccbee4a0dd3752558e2c16c57b3b
aymerickvalette <valaymerick@gmail.com>
2018-03-28 13:46:36 +0200

    Add country route

---
+++ app/routes/countries.js
@@ -0,0 +1,18 @@
+const mongoclient = require('mongodb').MongoClient;
+
+function getCountry(req, res) {
+    let countryName = req.query.name;
+    mongoclient.connect('mongodb://localhost:27017/airbase', function (err, client) {
+        if (err) {
+            throw err;
+        }
+        client.db('airbase').collection('airports').find({country:countryName.charAt(0).toUpperCase() + countryName.slice(1)}).toArray(function (err, result) {
+            if (err) {
+                throw err;
+            }
+            res.json(result);
+        });
+    });
+}
+
+module.exports = getCountry;
