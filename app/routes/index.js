1c95020ff1837fcff27771e0ae129f2b863935e9
aymerickvalette <valaymerick@gmail.com>
2018-03-28 13:48:21 +0200

    Add all routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,16 @@
 const routes = require('express').Router();
-const controller = require('./airports');
+const airports = require('./airports');
+const countries = require('./countries');
+const cities = require('./cities');
+const ids = require('./ids');
+const airportsRadar = require('./radar');
 
-routes.get('/airports', controller);
+
+routes.get('/airports', airports);
+routes.get('/airports/country', countries);
+routes.get('/airports/city', cities);
+routes.get('/airports/id', ids);
+routes.get('/airports/radar', airportsRadar);
+
 
 module.exports = routes;
