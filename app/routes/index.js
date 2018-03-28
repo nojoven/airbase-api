f23475520ab6c0196ceeea6dcbc3337d640dfaaf
aymerickvalette <valaymerick@gmail.com>
2018-03-29 00:12:22 +0200

    Remove unused routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,16 +1,7 @@
 const routes = require('express').Router();
-const airports = require('./airports');
-const countries = require('./countries');
-const cities = require('./cities');
-const ids = require('./ids');
-const airportsRadar = require('./radar');
+const airportController = require('../controllers/airports');
 
-
-routes.get('/airports', airports);
-routes.get('/airports/country', countries);
-routes.get('/airports/city', cities);
-routes.get('/airports/id', ids);
-routes.get('/airports/radar', airportsRadar);
-
+routes.get('/airports', airportController.getAirports);
+routes.get('/airports/:airportId', airportController.getAirportId);
 
 module.exports = routes;
