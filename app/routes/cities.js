ab86d1f2ee8fffa000ac6796e40ed9e213a2fe2c
aymerickvalette <valaymerick@gmail.com>
2018-03-28 13:46:32 +0200

    Add city route

---
+++ app/routes/cities.js
@@ -0,0 +1,18 @@
+const mongoclient = require('mongodb').MongoClient;
+
+function getCity(req, res) {
+    let cityName = req.query.name;
+    mongoclient.connect('mongodb://localhost:27017/airbase', function (err, client) {
+        if (err) {
+            throw err;
+        }
+        client.db('airbase').collection('airports').find({city:cityName.charAt(0).toUpperCase() + cityName.slice(1)}).toArray(function (err, result) {
+            if (err) {
+                throw err;
+            }
+            res.json(result);
+        });
+    });
+}
+
+module.exports = getCity;
