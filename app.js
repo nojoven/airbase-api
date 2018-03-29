e00ddae5cc22a79897bf2b760d13fd8fe5260f7c
aymerickvalette <valaymerick@gmail.com>
2018-03-29 20:49:27 +0200

    Remove unused headers

--- app.js
+++ app.js
@@ -9,13 +9,7 @@ app.use(function(req, res, next) {
     next();
 });  
 app.use('/', routes);
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    
-    next();
-});
+
 
 /**
  * Init the database connection and start the server
