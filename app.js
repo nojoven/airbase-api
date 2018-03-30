ae9f8d12d0c37f1842d017261a2e18721748eedd
aymerickvalette <valaymerick@gmail.com>
2018-03-30 21:09:31 +0200

    Change cors settings

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const routes = require('./app/routes');
 
 app.use(function(req, res, next) {  
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Origin', "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });  
